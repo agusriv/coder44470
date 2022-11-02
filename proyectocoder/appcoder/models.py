16e1916e9c3ae59a428191a784d6586b56b103ee
Leonel Gareis <leonelgareis@gmail.com>
2022-11-02 19:21:19 -0300

    FIX: Modelos
    Cambio de atributo de longitud

--- proyectocoder/appcoder/models.py
+++ proyectocoder/appcoder/models.py
@@ -8,17 +8,17 @@ class Curso(models.Model):
     camada = models.IntegerField() 
 
 class Estudiante(models.Model):
-    nombre = models.CharField(max_lngth=100)
-    apellido = models.CharField(max_lngth=100)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
     email = models.EmailField()
 
 class Profesor(models.Model):
-    nombre = models.CharField(max_lngth=100)
-    apellido = models.CharField(max_lngth=100)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
     email = models.EmailField()
-    profesion = models.CharField(max_lngth=100)
+    profesion = models.CharField(max_length=100)
 
 class Entregable(models.Model):
-    nombre = models.CharField(max_lngth=100)
+    nombre = models.CharField(max_length=100)
     fecha_de_entrega = models.DateField()
     entregado = models.BooleanField()
