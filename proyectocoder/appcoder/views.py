d5b42c9ea155d8fd3cbdcbbab3f7e2c1e6be545e
Leonel Gareis <leonelgareis@gmail.com>
2022-11-07 21:06:51 -0300

    Cambios
    Listado de registro de cursos en la vista cursos
    Se aplicaron migraciones a los modelos
    Se registraron los modelos al panel de administracion

--- proyectocoder/appcoder/views.py
+++ proyectocoder/appcoder/views.py
@@ -7,6 +7,12 @@ def inicio(request):
     return render(request, "appcoder/index.html")
 
 def cursos(request):
+    # Obtenes el listado de objetos en la BD
+    cursos = Curso.objects.all()
+
+    for curso in cursos:
+        print(curso.nombre)
+
     return render(request, "appcoder/cursos.html")
     
 def estudiantes(request):
@@ -16,7 +22,7 @@ def profesores(request):
     return render(request, "appcoder/profesores.html")
 
 def entregables(request):
-        return render(request, "appcoder/entregables.html")
+    return render(request, "appcoder/entregables.html")
 
 
 # def listado_cursos(request):
